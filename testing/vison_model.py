35adae4e79f817aaeb2adf369900186cf3f7062e
nagarjun s m <nagarjunsm00@gmail.com>
2024-11-27 22:20:45 +0530

    vison_model

---
+++ testing/vison_model.py
@@ -0,0 +1,36 @@
+from groq import Groq
+import base64
+
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = "sf.jpg"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+client = Groq()
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What's in this image?"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                },
+            ],
+        }
+    ],
+    model="llama-3.2-11b-vision-preview",
+)
+
+print(chat_completion.choices[0].message.content)
