4125548c685bb584bb3fd7976a53ef680310168d
nagarjun s m <nagarjunsm00@gmail.com>
2025-01-01 01:54:02 +0530

    add readme

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
 from utils.video_utils import read_video, save_video, detect_vehicles
-from object_tracker.tracker import Tracker
+from object_tracker.tracker_3 import Tracker
 
 def main():
+    frames = read_video("data/main.mp4")
 
-    frames = read_video("data/traffic_2.mp4")
-
-    #object tracking
-    obj_tracker = Tracker()
-    result = obj_tracker.detect_objects(frames)
-
-    output_frames = obj_tracker.draw_annotations(frames, result)
-
-
-    save_video(output_frames, "output/output2.avi")
-
-###### Main function for 1 image
-
-# def main():
-#
-#     frames = read_video("data/image.jpg")
-#
-#     #object tracking
-#     obj_tracker = Tracker()
-#     result = obj_tracker.detect_frame(frames)
-#     print(result)
-#
-#     # output_frames = obj_tracker.draw_annotations(frames, result)
-#     #
-#     #
-#     # save_video(output_frames, "output/output.avi")
+    track = Tracker()
+    result = track.process_video(frames)
 
 if __name__ == '__main__':
     main()
-
